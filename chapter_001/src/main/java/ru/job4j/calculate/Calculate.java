de6b85a8a4da3edfb3009cbe56ba9a8b4d200262
ArinaArina <arch_romantic@mail.ru>
2019-01-31 15:31:51 +0500

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
-
+/**
+ * Calculate
+ * Для вывода на консоль "Hello World"
+ * @author Arina Vakurova
+*/
+ 
 public class Calculate{
-	
+	/**
+	 * main 
+	 * Выводить на консоль "Hello World"
+	 * @param args - args
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Arina Vakurova (arch_romantic@mail.ru)
+ * @version $Id$
+ * @since 31.01.2019
+*/
+package ru.job4j.calculate;
