b20b729db98b950af292c24c8b8d5c8ad8696b65
ArinaArina <arch_romantic@mail.ru>
2019-02-12 18:29:34 +0500

    5.3. Построить шахматную доску в псевдографике. [#13559]

--- chapter_001/src/main/java/ru/job4j/loop/package-info.java
+++ chapter_001/src/main/java/ru/job4j/loop/package-info.java
@@ -2,6 +2,7 @@
  * Package for loop task.
  * Для вычисления факториала.
  * Для подсчёта суммы чётных чисел в диапазоне.
+ * Для построения шахматной доски в псевдографике.
  * @author Arina Vakurova (arch_romantic@mail.ru).
  * @version 1.
  * @since 10.02.2019.
